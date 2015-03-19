ec919cff9490018313864df7697e49db3f0f77a5
Sebastian Elsner <sebastian@risefx.com>
2015-03-19 10:48:35 +0100

    fix for python 2.6, dictionary comprehensions are only available in >2.7

--- qtawesome/iconic_font.py
+++ qtawesome/iconic_font.py
@@ -126,14 +126,20 @@ class IconicFont(QObject):
         directory: str or None, optional
             directory for font and charmap files
         """
+
+        def hook(o):
+            r={}
+            for k,v in o.iteritems():
+                r[k]=unichr(int(v, 16))
+            return r
+
         if directory is None:
             directory = os.path.join(
                 os.path.dirname(os.path.realpath(__file__)), 'fonts')
         with open(os.path.join(directory, ttf_filename), 'rb') as f:
             font_data = QByteArray(f.read())
         with open(os.path.join(directory, charmap_filename), 'r') as codes:
-            self.charmap[prefix] = json.load(codes,
-                                             object_hook=lambda o: {k: unichr(int(o[k], 16)) for k in o})
+            self.charmap[prefix] = json.load(codes, object_hook=hook)
         id_ = QFontDatabase.addApplicationFontFromData(font_data)
         loadedFontFamilies = QFontDatabase.applicationFontFamilies(id_)
         if(loadedFontFamilies):
