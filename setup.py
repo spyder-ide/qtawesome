143b7b493db3b00967c614f983e2f21f9669c4f7
Sylvain Corlay <sylvain.corlay@gmail.com>
2015-10-05 17:19:14 -0400

    Update long description and fix badges

--- setup.py
+++ setup.py
@@ -15,16 +15,37 @@ def read_version():
             if "__version__" in l:
                 return l.split("=")[1].strip().replace("'", '').replace('"', '')
 
+LONG_DESCRIPTION = """
+.. image:: https://img.shields.io/pypi/v/QtAwesome.svg
+   :target: https://pypi.python.org/pypi/QtAwesome/
+   :alt: Latest PyPI version
 
-def readme():
-    return str(open('README.rst').read())
+.. image:: https://img.shields.io/pypi/dm/QtAwesome.svg
+   :target: https://pypi.python.org/pypi/QtAwesome/
+   :alt: Number of PyPI downloads
 
+QtAwesome - Iconic Fonts in PyQt and PySide applications
+========================================================
+
+QtAwesome enables iconic fonts such as Font Awesome and Elusive Icons in PyQt and PySide applications.
+
+It is a port to Python - PyQt / PySide of the QtAwesome C++ library by Rick Blommers.
+
+.. code-block:: python
+
+    # Get icons by name.
+    fa_icon = qta.icon('fa.flag')
+    fa_button = QtGui.QPushButton(fa_icon, 'Font Awesome!')
+
+    asl_icon = qta.icon('ei.asl')
+    elusive_button = QtGui.QPushButton(asl_icon, 'Elusive Icons!')
+"""
 
 setup(
     name='QtAwesome',
     version=read_version(),
     description='FontAwesome icons in PyQt and PySide applications',
-    long_description=readme(),
+    long_description=LONG_DESCRIPTION,
     author='Sylvain Corlay',
     author_email='sylvain.corlay@gmail.com',
     license='MIT',
