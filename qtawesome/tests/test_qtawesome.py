fc2c750e54c75593b83a33c0596e589fd5e879f2
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-01-24 15:22:28 -0500

    Add test_unique_font_family_name
    Test that each font used by qtawesome has a unique name.
    Regression test for Issue #107

--- qtawesome/tests/test_qtawesome.py
+++ qtawesome/tests/test_qtawesome.py
@@ -7,10 +7,34 @@ import subprocess
 # Test Library imports
 import pytest
 
+# Local imports
+import qtawesome as qta
+from qtawesome.iconic_font import IconicFont
+
+
 def test_segfault_import():
     output_number = subprocess.call('python -c "import qtawesome '
                                     '; qtawesome.icon()"', shell=True)
     assert output_number == 0
-    
+
+
+def test_unique_font_family_name(qtbot):
+    """
+    Test that each font used by qtawesome has a unique name.
+
+    Regression test for Issue #107
+    """
+    resource = qta._instance()
+    assert isinstance(resource, IconicFont)
+
+    prefixes = list(resource.fontname.keys())
+    assert prefixes
+
+    fontnames = set(resource.fontname.values())
+    assert fontnames
+
+    assert len(prefixes) == len(fontnames)
+
+
 if __name__ == "__main__":
     pytest.main()
