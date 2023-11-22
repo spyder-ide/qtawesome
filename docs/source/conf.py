70809249c889c3dae5a5b0e99d11c4f9a6323538
dalthviz <16781833+dalthviz@users.noreply.github.com>
2023-11-22 17:06:52 -0500

    Add config to install package for the docs build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -89,7 +89,7 @@ release = _release['__version__']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+# language = None
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
