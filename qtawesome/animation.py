5545a90835bb49515583502921bf0a55409946d3
dalthviz <16781833+dalthviz@users.noreply.github.com>
2026-05-12 13:21:15 -0500

    pre-commit hook mypy addition and animation info attribute typing

--- qtawesome/animation.py
+++ qtawesome/animation.py
@@ -19,7 +19,7 @@ class Spin:
         self.step = step
         self.autostart = autostart
 
-        self.info = {}
+        self.info: dict[QWidget, tuple[QTimer, int, int]] = {}
 
     def _update(self):
         if self.parent_widget in self.info:
